9a201d6e8d6f2d3c977e5af6eaa3696ec6151f11
markdave123-py <nwaekwudavid@gmail.com>
2023-09-14 00:18:43 +0100

    Three Sum

---
+++ ThreeSum.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        out = []
+
+        nums.sort()
+
+        for i, num in enumerate(nums):
+            if i > 0 and num == nums[i -1]:
+                continue
+
+            l, r = i + 1, len(nums) - 1
+
+            while l < r:
+                threeSum = num + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+
+                else:
+                    out.append([num, nums[l], nums[r]])
+                    l +=1
+
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
+
+        return out
+
+            
