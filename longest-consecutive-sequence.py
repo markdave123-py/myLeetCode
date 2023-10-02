1fcbb7f39c359e394bf31c97fe1cbb109bdebfa4
markdave123-py <nwaekwudavid@gmail.com>
2023-10-02 02:04:52 +0100

    changes

--- longest-consecutive-sequence.py
+++ longest-consecutive-sequence.py
@@ -22,3 +22,17 @@ class Solution(object):
             longest = max(longest, next - n)
         
         return longest
+
+
+
+        numSet = set(nums)
+        longest = 0
+
+        for num in nums:
+            if (num - 1) not in numSet:
+                length = 0
+                while (num + length) in numSet:
+                    length += 1
+                longest = max(longest, length)
+
+        return longest
