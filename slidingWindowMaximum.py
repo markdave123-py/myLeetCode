6e531af8383bd140fc0fc4136c7d6147e9067a8b
markdave123-py <nwaekwudavid@gmail.com>
2023-11-29 23:53:05 +0100

    sliding window maximax

--- slidingWindowMaximum.py
+++ slidingWindowMaximum.py
@@ -1,18 +1,50 @@
+# class Solution(object):
+#     def maxSlidingWindow(self, nums, k):
+#         """
+#         :type nums: List[int]
+#         :type k: int
+#         :rtype: List[int]
+#         """
+#         l = 0
+#         r = k
+#         output = []
+
+#         while r <= len(nums):
+#             maxN = max(nums[l:r])
+#             output.append(maxN)
+#             l += 1
+#             r += 1
+
+#         return output
+
 class Solution(object):
     def maxSlidingWindow(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        l = 0
-        r = k
+        l = r = 0
         output = []
+        q = collections.deque()
 
-        while r <= len(nums):
-            maxN = max(nums[l:r])
-            output.append(maxN)
-            l += 1
-            r += 1
+        for r in range(len(nums)):
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            if l > q[0]:
+                q.popleft()
+
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l += 1
 
         return output
+
+
+           # while r < len(nums):
+        #     maxN = max(nums[l:r])
+        #     output.append(maxN)
+        #     l += 1
+        #     r += 1
+
+        # return output
+
+
+
