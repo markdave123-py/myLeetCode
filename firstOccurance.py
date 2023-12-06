a5ac5a212446ea027d88459cfb439be87b945316
markdave123-py <nwaekwudavid@gmail.com>
2023-12-06 23:30:54 +0100

    first occurance

---
+++ firstOccurance.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        arr = 0
+        l = 0
+        i = 0
+        currf = s[0]
+
+        while i < len(t):
+            arr.apend(t[i])
+            while l < len(s):
+                if s[l] == t[i]:
+                    arrr.pop()
+                    i += 1
+                    l += 1
+
+                else:
+                    l += 1
+        
