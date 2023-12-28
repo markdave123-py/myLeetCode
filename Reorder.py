98c88f6bc8cadff789fc3bd1c69a67265135dfaf
markdave123-py <nwaekwudavid@gmail.com>
2023-12-28 18:26:58 +0100

    Reorder linkedlist

---
+++ Reorder.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reorderList(self, head):
+        l, r = head, head.next
+        while r and r.next:
+            l, r = l.next, r.next.next
+
+        second = l.next
+        prev = l.next = None
+
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
