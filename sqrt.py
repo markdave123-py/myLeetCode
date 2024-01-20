c6db910b15e02b0aac8dca629e219d6c313a06da
markdave123-py <nwaekwudavid@gmail.com>
2024-01-20 23:24:01 +0100

    sqrt

---
+++ sqrt.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+        l,r = 0,x
+
+        while l <= r:
+            mid = (l + r)//2
+
+            if mid * mid == x:
+                return mid
+
+            elif mid * mid < x:
+                l = mid + 1
+
+            else:
+                r = mid -1
+
+        return r
+        
